90920a073184cd128e1ff696bb9e4def7063c4a3
Your Name <you@example.com>
2016-04-13 00:08:45 +0300

    nightTest

--- main.c
+++ main.c
@@ -2,12 +2,72 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <signal.h>
+#include <fcntl.h>
 
-int GenID, TextID;
+#define LENGTH 128
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) //-nkey -l <number>
 {
-	printf("Hello!");
-	return 0;
+	int PID;
+	int pipefd[2];
+	int fd;
+	int len; //key length
+	char kk[LENGTH+1];
+
+	if (pipe(pipefd) < 0)
+	{
+		perror("Pipe error");
+		exit(0);
+	}
+
+	if ((PID = fork()) < 0)
+	{
+		perror("Fork error");
+		exit(0);	
+	}
+
+	//open key in main program!
+
+	if (PID == 0) //generator
+	{
+		close(pipefd[0]); //close read
+		//generate a key
+		int i;
+		//random init
+		time_t ti;
+		char st[128];
+		(void) time(&ti);
+		(void) initstate((unsigned int)(ti + getpid()), st, sizeof(st));
+		(void) setstate(st);
+		//and generate!
+		for (i = 0; i < LENGTH; i++)
+			kk[i] = random()%128;
+		//send it
+		write(pipefd[1], kk, LENGTH);
+		close(pipefd[1]);
+		_exit(0);
+	}
+	else //main program
+	{
+		close(pipefd[1]); //close write
+		read(pipefd[0], kk, LENGTH);
+		printf("Key: %s\n\n, Length = %d", kk, sizeof(kk));
+		close(pipefd[0]);
+		exit(0);
+	}
+	/*
+	close(fd);
+	remove("key");
+	//no file, create it
+	if ((fd = open("key", O_RDWR | O_CREAT)) < 0)
+	{
+		perror("Key error");
+		close(fd);
+		_exit(0);
+	}
+	close(fd);
+	*/
+	
+	//waitpid(PID);	
+	return 1;
 }
